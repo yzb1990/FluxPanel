21b6201089b27924a6d5712bcb5b15a51a8f071d
WuBo <985507966@qq.com>
2025-02-28 17:28:14 +0800

    [feature]添加docker启动

--- flux-frontend/vite.config.js
+++ flux-frontend/vite.config.js
@@ -5,7 +5,7 @@ import createVitePlugins from './vite/plugins'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_ENV, VITE_APP_PROXY_API } = env
+  const { VITE_APP_ENV } = env
   return {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: VITE_APP_PROXY_API,
+          target: 'http://127.0.0.1:9099/',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
