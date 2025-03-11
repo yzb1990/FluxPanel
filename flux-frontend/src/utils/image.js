09308b65823d881a5654895a8075f8f4da8ffdf6
WuBo <985507966@qq.com>
2025-03-11 16:42:11 +0800

    [bugfix]图片地址判空问题

--- flux-frontend/src/utils/image.js
+++ flux-frontend/src/utils/image.js
@@ -3,7 +3,7 @@
  * 获取字典数据
  */
 export function fullUrl(url) {
-    if (url.startsWith('http')) {
+    if (url && url.startsWith('http')) {
         return url
     }
     return import.meta.env.VITE_APP_BASE_API + url
